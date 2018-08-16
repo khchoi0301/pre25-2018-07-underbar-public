2752067c37dd810f9f64b65d62a7954326ad3e3d
kihong choi <khchoi0301@gmail.com>
2018-08-16 22:11:40 +0900

    Work on Reduce

--- src/underbar.js
+++ src/underbar.js
@@ -161,21 +161,29 @@
 
   // Return the results of applying an iterator to each element.
   _.map = function(collection, iterator) {
-
-    if(Array.isArray(collection)){
+    /*
+    //if(Array.isArray(collection)){
       var arr=[]
       for(var i=0;i<collection.length;i++){
-        console.log(iterator(collection[i]),collection)
+        //console.log(iterator(collection[i]),collection)
         arr.push(iterator(collection[i]))
-        console.log(arr)
+        //console.log(arr)
       }
       return arr;
+      */
+   /* 
     } else {    
       for (var key in collection){
         iterator(collection[key],key,collection);
       }
     }
-   // _.each(collection,iterator)
+    */  
+   var arr=[]
+    _.each(collection,function(item){
+      arr.push(iterator(item))
+    })
+    return arr;
+
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
@@ -220,6 +228,21 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+   // _.each(collection,iterator)
+  
+   
+    
+    for(var i=0;i<collection.length;i++){
+      console.log(collection,i,':',collection[i],accumulator)
+      if(accumulator!==undefined){
+        accumulator=iterator(accumulator,collection[i],i,collection);
+        //accumulator = iterator(accumulator, item)
+      } else {
+        accumulator= collection[0];
+      }     
+      console.log(collection,i,':',collection[i],accumulator)
+    } return accumulator
+    
   };
 
   // Determine if the array or object contains a given value (using `===`).
